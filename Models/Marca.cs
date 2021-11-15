65ce1fb68ca3ab6f75cd7c07554e908b0630a10a
ghnoob <ptnchk.rodrigo@gmail.com>
2021-11-15 15:24:53 -0300

    add Marca model

---
+++ Models/Marca.cs
@@ -0,0 +1,15 @@
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace FinalLaboratorio4.Models
+{
+    public class Marca
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(20)]
+        [MaxLength(20)]
+        public string Descripcion { get; set; }
+    }
+}
