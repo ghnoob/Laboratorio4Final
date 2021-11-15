ae27b9ea0a7ae4ec31b3291e78d129a85198ed92
ghnoob <ptnchk.rodrigo@gmail.com>
2021-11-15 15:25:42 -0300

    migrate Marca

---
+++ Data/Migrations/20211115182340_Marcas.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FinalLaboratorio4.Data.Migrations
+{
+    public partial class Marcas : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Marcas",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Descripcion = table.Column<string>(type: "TEXT", maxLength: 20, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Marcas", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Marcas");
+        }
+    }
+}
