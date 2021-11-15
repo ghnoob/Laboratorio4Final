c8b333fd9804f8996fbb96a8fd01adfd910d4b6b
ghnoob <ptnchk.rodrigo@gmail.com>
2021-11-15 15:21:52 -0300

    add Categoria to DBContext

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,16 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using FinalLaboratorio4.Models;
 
 namespace FinalLaboratorio4.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-            : base(options)
-        {
-        }
+            : base(options) {}
+
+
+        public DbSet<Categoria> Categorias { get; set; }
     }
 }
