ad467414142fdf078d8e5a79d98008c5a4360921
ghnoob <ptnchk.rodrigo@gmail.com>
2021-11-17 23:13:59 -0300

    add relation between productos and proveedores

--- Controllers/ProductosController.cs
+++ Controllers/ProductosController.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,6 +8,7 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using FinalLaboratorio4.Data;
 using FinalLaboratorio4.Models;
+using FinalLaboratorio4.Models.ProductoViewModels;
 using FinalLaboratorio4.Helpers;
 
 namespace FinalLaboratorio4.Controllers
@@ -54,6 +56,9 @@ namespace FinalLaboratorio4.Controllers
         {
             ViewData["CategoriaId"] = new SelectList(_context.Categorias, "Id", "Descripcion");
             ViewData["MarcaId"] = new SelectList(_context.Marcas, "Id", "Descripcion");
+
+            PopulateProveedoresData();
+
             return View();
         }
 
@@ -62,7 +67,7 @@ namespace FinalLaboratorio4.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,Nombre,Precio,Descripcion,Imagen,CategoriaId,MarcaId")] Producto producto)
+        public async Task<IActionResult> Create([Bind("Id,Nombre,Precio,Descripcion,Imagen,CategoriaId,MarcaId")] Producto producto, string[] selectedProveedores)
         {
             if (ModelState.IsValid)
             {
@@ -73,6 +78,10 @@ namespace FinalLaboratorio4.Controllers
                         Path.Combine(_env.WebRootPath, "img", "productos")
                     )
                 );
+
+                producto.Proveedores = new List<Proveedor>();
+
+                UpdateProductoProveedores(selectedProveedores, producto);
 
                 _context.Add(producto);
                 await _context.SaveChangesAsync();
@@ -91,13 +100,20 @@ namespace FinalLaboratorio4.Controllers
                 return NotFound();
             }
 
-            var producto = await _context.Productos.FindAsync(id);
+            Producto producto = await _context.Productos
+                .Include(p => p.Proveedores)
+                .FirstOrDefaultAsync(p => p.Id == id);
+
             if (producto == null)
             {
                 return NotFound();
             }
+
             ViewData["CategoriaId"] = new SelectList(_context.Categorias, "Id", "Descripcion", producto.CategoriaId);
             ViewData["MarcaId"] = new SelectList(_context.Marcas, "Id", "Descripcion", producto.MarcaId);
+
+            PopulateProveedoresData(producto);
+
             return View(producto);
         }
 
@@ -106,30 +122,48 @@ namespace FinalLaboratorio4.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,Nombre,Precio,Descripcion,UrlImagen,Favorito,CategoriaId,MarcaId")] Producto producto)
+        public async Task<IActionResult> Edit(int? id, string[] selectedProveedores)
         {
-            if (id != producto.Id)
+            if (id == null)
             {
                 return NotFound();
             }
 
-            if (ModelState.IsValid)
+            Producto producto = await _context.Productos
+                .Include(p => p.Proveedores)
+                .FirstOrDefaultAsync(p => p.Id == id);
+
+            if (
+                await TryUpdateModelAsync<Producto>(
+                    producto,
+                    "",
+                    p => p.Nombre,
+                    p => p.Precio,
+                    p => p.Descripcion,
+                    p => p.CategoriaId,
+                    p => p.MarcaId,
+                    p => p.Imagen
+                )
+            )
             {
+                if (producto.Imagen != null)
+                {
+                    FileManager.Delete(Path.Combine(_env.WebRootPath, producto.UrlImagen));
+
+                    producto.UrlImagen = Path.GetRelativePath(
+                        _env.WebRootPath,
+                        FileManager.Create(
+                            producto.Imagen,
+                            Path.Combine(_env.WebRootPath, "img", "productos")
+                        )
+                    );
+                }
+
+                UpdateProductoProveedores(selectedProveedores, producto);
+                PopulateProveedoresData(producto);
+
                 try
                 {
-                    if (producto.Imagen != null)
-                    {
-                        FileManager.Delete(Path.Combine(_env.WebRootPath, producto.UrlImagen));
-
-                        producto.UrlImagen = Path.GetRelativePath(
-                            _env.WebRootPath,
-                            FileManager.Create(
-                                producto.Imagen,
-                                Path.Combine(_env.WebRootPath, "img", "productos")
-                            )
-                        );
-                    }
-
                     _context.Update(producto);
                     await _context.SaveChangesAsync();
                 }
@@ -149,6 +183,65 @@ namespace FinalLaboratorio4.Controllers
             ViewData["CategoriaId"] = new SelectList(_context.Categorias, "Id", "Descripcion", producto.CategoriaId);
             ViewData["MarcaId"] = new SelectList(_context.Marcas, "Id", "Descripcion", producto.MarcaId);
             return View(producto);
+        }
+
+        private void PopulateProveedoresData()
+        {
+            DbSet<Proveedor> proveedores = _context.Proveedores;
+            List<AssignedProveedorData> viewModel = proveedores.Select(
+                p => new AssignedProveedorData()
+                {
+                    Id = p.Id,
+                    Nombre = p.Nombre,
+                    Asignado = false
+                }
+            ).ToList();
+
+            ViewData["Proveedores"] = viewModel;
+        }
+
+        private void PopulateProveedoresData(Producto producto)
+        {
+            DbSet<Proveedor> proveedores = _context.Proveedores;
+            HashSet<int> productoProveedores = new(producto.Proveedores.Select(p => p.Id));
+
+            List<AssignedProveedorData> viewModel = proveedores.Select(
+                p => new AssignedProveedorData()
+                {
+                    Id = p.Id,
+                    Nombre = p.Nombre,
+                    Asignado = productoProveedores.Contains(p.Id)
+                }
+            ).ToList();
+
+            ViewData["Proveedores"] = viewModel;
+        }
+
+        private void UpdateProductoProveedores(string[] proveedores, Producto producto)
+        {
+            if (proveedores == null)
+            {
+                producto.Proveedores = new List<Proveedor>();
+                return;
+            }
+
+            HashSet<string> proveedoresHS = new(proveedores);
+            HashSet<int> productoProveedores = new(producto.Proveedores.Select(p => p.Id));
+
+            foreach (Proveedor proveedor in _context.Proveedores)
+            {
+                if (
+                    proveedoresHS.Contains(proveedor.Id.ToString())
+                    && !productoProveedores.Contains(proveedor.Id)
+                )
+                {
+                    producto.Proveedores.Add(proveedor);
+                }
+                else if (productoProveedores.Contains(proveedor.Id))
+                {
+                    producto.Proveedores.Remove(proveedor);
+                }
+            }
         }
 
         // GET: Productos/Delete/5
---
+++ Models/ProductoViewModels/ProductosProveedoresData.cs
@@ -0,0 +1,9 @@
+namespace FinalLaboratorio4.Models.ProductoViewModels
+{
+    public class AssignedProveedorData
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public bool Asignado { get; set; }
+    }
+}
