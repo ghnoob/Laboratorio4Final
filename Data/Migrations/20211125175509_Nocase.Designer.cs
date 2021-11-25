4736e4507e6bc7046c8d6155f545b4d4cd390885
ghnoob <ptnchk.rodrigo@gmail.com>
2021-11-25 15:25:54 -0300

    migrate model

--- Data/Migrations/20211125175509_Nocase.Designer.cs
+++ Data/Migrations/20211125175509_Nocase.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using FinalLaboratorio4.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace FinalLaboratorio4.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20211125175509_Nocase")]
+    partial class Nocase
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Data/Migrations/20211125175509_Nocase.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FinalLaboratorio4.Data.Migrations
+{
+    public partial class Nocase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Nombre",
+                table: "Proveedores",
+                type: "TEXT",
+                maxLength: 20,
+                nullable: false,
+                collation: "NOCASE",
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldMaxLength: 20);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Nombre",
+                table: "Productos",
+                type: "TEXT",
+                maxLength: 50,
+                nullable: false,
+                collation: "NOCASE",
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldMaxLength: 50);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Descripcion",
+                table: "Marcas",
+                type: "TEXT",
+                maxLength: 20,
+                nullable: false,
+                collation: "NOCASE",
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldMaxLength: 20);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Nombre",
+                table: "Proveedores",
+                type: "TEXT",
+                maxLength: 20,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldMaxLength: 20,
+                oldCollation: "NOCASE");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Nombre",
+                table: "Productos",
+                type: "TEXT",
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldMaxLength: 50,
+                oldCollation: "NOCASE");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Descripcion",
+                table: "Marcas",
+                type: "TEXT",
+                maxLength: 20,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldMaxLength: 20,
+                oldCollation: "NOCASE");
+        }
+    }
+}
