85ced5aa6babb317fa2a68ea799208958928b0c5
ghnoob <ptnchk.rodrigo@gmail.com>
2021-11-26 00:51:26 -0300

    show favorites in home page

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,26 +1,38 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
+﻿using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using FinalLaboratorio4.Models;
+using FinalLaboratorio4.Data;
 
 namespace FinalLaboratorio4.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index(int? pageNumber)
         {
-            return View();
+            int pageSize = 16;
+
+            return View(await PaginatedList<Producto>.CreateAsync(
+                _context.Productos
+                    .AsNoTracking()
+                    .Where(p => p.Favorito)
+                    .Include(p => p.Categoria)
+                    .OrderBy(p => p.Nombre),
+                pageNumber ?? 1,
+                pageSize
+            ));
         }
 
         public IActionResult Privacy()
