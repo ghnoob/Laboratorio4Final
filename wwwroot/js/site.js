0a5d8988ee8a1f11af198abe682a73fd3ab76dfd
ghnoob <ptnchk.rodrigo@gmail.com>
2021-11-27 23:37:56 -0300

    add js function to toggle favorites

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,3 +1,22 @@
 ﻿$('img').on('error', function () {
     $(this).attr('src', '/img/placeholder.jpg');
 });
+
+$('.js-toggle-favorite').on('click', async function () {
+    const id = $(this).data('id');
+
+    const res = await fetch(
+        `/Productos/ToggleFavorite/${id}`,
+        {
+            method: 'POST',
+            // mode: 'same-origin',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded'
+            }
+        }
+    );
+
+    if (res.ok) {
+        $(`#favorite-${id}`).toggleClass('far fas');
+    }
+});
